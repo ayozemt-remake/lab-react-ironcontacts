57a7588f1695b5a2b06187724102f8ef0baac816
ayozemt <ayozemt@gmail.com>
2023-11-28 19:17:57 +0100

    Iteration 3

--- src/App.js
+++ src/App.js
@@ -1,40 +1,56 @@
 import "./App.css";
-import contacts from "./contacts.json";
+import contactsData from "./contacts.json";
 import { useState } from "react";
 
 function App() {
-  const [allContacts, setAllContacts] = useState(contacts.slice(5, 10));
+  const [allContacts, setAllContacts] = useState(contactsData.slice(5, 10));
+
+  const addRandomContact = () => {
+    const randomContact =
+      contactsData[Math.floor(Math.random() * contactsData.length)];
+    if (!allContacts.includes(randomContact)) {
+      setAllContacts([randomContact, ...allContacts]);
+    }
+  };
+
   return (
     <div className="App">
       <h1>IronContacts</h1>
+      <button onClick={addRandomContact} style={{ cursor: "pointer" }}>
+        Add Random Contact
+      </button>
+      <hr />
       <table>
-        <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-          <th>Won Oscar</th>
-          <th>Won Emmy</th>
-        </tr>
-
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
         {allContacts.map((contact) => {
           return (
-            <tr key={contact.id}>
-              <td>
-                <img
-                  src={contact.pictureUrl}
-                  alt={contact.name}
-                  style={{ width: "150px" }}
-                />
-              </td>
-              <td>
-                <p>{contact.name}</p>
-              </td>
-              <td>
-                <p>{contact.popularity.toFixed(2)}</p>
-              </td>
-              <td>{contact.wonOscar ? <p>🏆</p> : <p></p>}</td>
-              <td>{contact.wonEmmy ? <p>🏆</p> : <p></p>}</td>
-            </tr>
+            <tbody>
+              <tr key={contact.id}>
+                <td>
+                  <img
+                    src={contact.pictureUrl}
+                    alt={contact.name}
+                    style={{ width: "150px" }}
+                  />
+                </td>
+                <td>
+                  <p>{contact.name}</p>
+                </td>
+                <td>
+                  <p>{contact.popularity.toFixed(2)}</p>
+                </td>
+                <td>{contact.wonOscar ? <p>🏆</p> : <p></p>}</td>
+                <td>{contact.wonEmmy ? <p>🏆</p> : <p></p>}</td>
+              </tr>
+            </tbody>
           );
         })}
       </table>
